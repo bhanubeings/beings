065a5a902df11b064df98f04740e2d1ad8bf6478
bhanubeings <53905409+bhanubeings@users.noreply.github.com>
2019-08-20 12:41:57 +0530

    done with the base program

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+a=random.randrange(1,2)
+print(a)
+num=int(input("enter the num"))
+if(a==num):
+    print("sadhinichinav poo iga")
+else:
+    if(num==1):
+        print('wow bagane kotinav gane pakakpoo')
+    if(num==2):
+        print('ammoo saalll anna pakakpoo ')
